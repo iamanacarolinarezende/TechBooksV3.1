be5475b58f7cda2c1b00840296d19887d534e62d
Ana Carolina Rezende <72672907+iamanacarolinarezende@users.noreply.github.com>
2024-10-07 12:03:22 -0400

    Categories and Layout fixes

--- TechBooks.WebUI/wwwroot/js/site.js
+++ TechBooks.WebUI/wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function removeConfirmation() {
+    return confirm("Do you confirm this deletion?");
+}
+
